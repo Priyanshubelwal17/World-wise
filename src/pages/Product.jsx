a37e67aebdf55dad747ee37421547cadfc3e5c95
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-11 18:33:10 +0530

    Link btw routes with <Link/> and <NavLink/>

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,11 @@
-function Product() {
-  return <div>Product</div>;
-}
+import PageNav from "../components/PageNav";
 
+function Product() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Product</h1>
+    </div>
+  );
+}
 export default Product;
