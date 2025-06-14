5c0c2e70b69bccd07a21a88657fd00abb03ec5b6
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-14 16:17:08 +0530

    Finish the city view

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import Flag from "./Flag";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,21 +12,24 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
   return (
-    <Link
-      className={styles.cityItem}
-      to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-    >
-      <li className={styles.cityItem}>
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        } `}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>
           <Flag flag={emoji} />
         </span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
         <button className={styles.deleteBtn}>&times;</button>
-      </li>
-    </Link>
+      </Link>
+    </li>
   );
 }
 
--- src/components/Flag.jsx
+++ src/components/Flag.jsx
@@ -1,7 +1,10 @@
-function Flag({ flag }) {
+function Flag({ flag = "" }) {
+  if (!flag) return null; // or show default flag
+
   const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
     .map((char) => String.fromCharCode(char - 127397).toLowerCase())
     .join("");
+
   return (
     <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
   );
